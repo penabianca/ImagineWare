f27736344d6b0558a2b95083e88c44ef2342f9fa
Aime Ngongang <aimechicago5@yahoo.fr>
2013-10-05 13:00:25 -0700

    trying to fix the issue with deprecation

---
+++ spec/request/static_pages_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+    it "should have the content 'Hospital Linked'" do
+      visit '/static_pages/home'
+      expect(page).to have_content ('Hospital Linked')
+    end
+  end
+  describe "Help page" do
+    it "should have the content 'Help' " do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  describe "About page" do
+    it "should have the content 'About Us' " do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
+  end
+end
