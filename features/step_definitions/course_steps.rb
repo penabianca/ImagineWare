adccfb32e4092034124b310006051062b8bb5d86
Alex Hu <alexhu@berkeley.edu>
2013-10-28 11:26:19 -0700

    Fixed indentation

--- features/step_definitions/course_steps.rb
+++ features/step_definitions/course_steps.rb
@@ -1,7 +1,5 @@
 Given /the following courses exist/ do |courses_table|
-    courses_table.hashes.each do |course|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that course to the database here.
-    Course.create!(course)
+  courses_table.hashes.each do |course|
+      Course.create!(course)
   end
 end
