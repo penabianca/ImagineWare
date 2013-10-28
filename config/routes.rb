dd11e6b2a74acc22dad0cd0b6c96140e9474e6db
Alex Hu <alexhu@berkeley.edu>
2013-10-28 10:56:03 -0700

    reordered routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 HospitalLink::Application.routes.draw do
-
-  
-
-
+  root  to: 'static_pages#home', via: 'get'
 
   get "users/new"
   match '/signup', to: 'users#new' , via: 'get'
   match '/help', to:  'static_pages#help' , via: 'get'
   match '/about', to:   'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact' , via: 'get'
-  resources :course
+  resources :courses
   resources :hospitals
-  root  to: 'static_pages#home', via: 'get'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
