052567d1596f275ca0f2301fb8ed8f4ef436f36b
Alex Hu <alexhu@berkeley.edu>
2013-11-06 05:37:45 -0800

    Removed extraneous submission code

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,14 +1,11 @@
 class SubmissionsController < ApplicationController
-
   def show
     id = params[:id]
     @submission = Submission.find(id) 
-    # will render app/views/Courses/show.<extension> by default
   end
 
   def index
     @submissions = Submission.where("student_id = #{session[:current_user]}").to_a
-    
   end
 
   def create
@@ -16,12 +13,4 @@ class SubmissionsController < ApplicationController
     flash[:notice] = "Your submission for #{Course.find(@submission.course_id).title} was successful"
     redirict_to user_path
   end
-=begin
-  def update
-    @submission = Submission.find params[:id]
-    @submission.update_attributes!(params[:Submission])
-    flash[:notice] = "#{User.find(@submission.user_id).first_name}'s submission for #{Course.find(@submission.course_id).title} has been graded"
-    redirect_to course_path(@course)
-  end
-=end
 end
