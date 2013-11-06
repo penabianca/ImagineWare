dd6b4fcbd2018c9438f6a39f1d72f48252cbf755
Alex Hu <alexhu@berkeley.edu>
2013-11-06 06:42:29 -0800

    submission step definitions

---
+++ features/step_definitions/submission_steps.rb
@@ -0,0 +1,19 @@
+When(/^I submit my assignment$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)" message$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the following submission: "(.*?)", "(.*?)"$/) do |arg1, arg2|
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^that my "(.*?)" submission gets graded with a "(.*?)"$/) do |arg1, arg2|
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^I press on "(.*?)"$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
