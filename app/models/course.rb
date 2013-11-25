98581b7a0e5d16e1c43a11de4a90ed759ff3861e
Alex Hu <alexhu@berkeley.edu>
2013-11-24 22:54:11 -0800

    Added markdown rendering to course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,16 @@
 class Course < ActiveRecord::Base
   attr_accessible :title, :content, :tag_list
   acts_as_taggable_on :tags
+
+  #getter
+  def render_content
+    require 'redcarpet'
+    renderer    = Redcarpet::Render::HTML.new
+    extensions  = {fenced_code_blocks: true}
+    redcarpet   = Redcarpet::Markdown.new(renderer, extensions)
+
+    @rendered_body = redcarpet.render self.content
+    return @rendered_body
+  end
+
 end
