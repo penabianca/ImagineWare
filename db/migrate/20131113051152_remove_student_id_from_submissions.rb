31669cb4ec7c6481ad8265a8fd7a50b91c94005d
penabianca <aimechicago5@yahoo.fr>
2013-12-10 01:33:09 -0800

    changes to remove_student_id_from_submissions

--- db/migrate/20131113051152_remove_student_id_from_submissions.rb
+++ db/migrate/20131113051152_remove_student_id_from_submissions.rb
@@ -1,9 +1,9 @@
 class RemoveStudentIdFromSubmissions < ActiveRecord::Migration
   def up
-    remove_column :submissions, :student_id
+    #remove_column :submissions, :student_id
   end
 
   def down
-    add_column :submissions, :student_id, :integer
+    #add_column :submissions, :student_id, :integer
   end
 end
