228401909878889e8b97b508371bf8f5af330be9
penabianca <aimechicago5@yahoo.fr>
2013-12-10 06:46:10 -0800

    added deletion dependency between course and submission

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ActiveRecord::Base
   attr_accessible :title,:points, :content, :tag_list
   acts_as_taggable_on :tags
-  has_many :submissions, inverse_of: :course
+  has_many :submissions, inverse_of: :course, :dependent => :destroy
   def self.all_grades
     %W(A A- B+ B B- C+ C C- D+ D D- F)
   end
