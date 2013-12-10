5aa59d2702055bde60d0d8ed37c662d3016ba942
penabianca <aimechicago5@yahoo.fr>
2013-12-10 01:23:43 -0800

    more changes addforeignkeys migration

--- db/migrate/20131110220042_add_foreign_keys_to_submissions.rb
+++ db/migrate/20131110220042_add_foreign_keys_to_submissions.rb
@@ -1,6 +1,6 @@
 class AddForeignKeysToSubmissions < ActiveRecord::Migration
   def change
-    remove_column :submissions, :course_id, :integer
-    remove_column :submissions, :student_id, :integer
+    #remove_column :submissions, :course_id, :integer
+    #remove_column :submissions, :student_id, :integer
   end
 end
