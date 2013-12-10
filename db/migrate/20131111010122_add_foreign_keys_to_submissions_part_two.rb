c7d56e5f048028c309efa68997993a571c6f571e
penabianca <aimechicago5@yahoo.fr>
2013-12-10 01:27:46 -0800

    changes to foreign_keys_to_submissions_part_two

--- db/migrate/20131111010122_add_foreign_keys_to_submissions_part_two.rb
+++ db/migrate/20131111010122_add_foreign_keys_to_submissions_part_two.rb
@@ -1,6 +1,6 @@
 class AddForeignKeysToSubmissionsPartTwo < ActiveRecord::Migration
   def change
-    add_column :submissions, :course_id, :integer
-    add_column :submissions, :user_id, :integer
+    #add_column :submissions, :course_id, :integer
+    #add_column :submissions, :user_id, :integer
   end
 end
