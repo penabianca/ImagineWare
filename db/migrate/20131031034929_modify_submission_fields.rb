64216fbd83e3be188827b2edf12d03f68c743f35
penabianca <aimechicago5@yahoo.fr>
2013-12-10 01:14:24 -0800

    changed modifysubmissionfields migration

--- db/migrate/20131031034929_modify_submission_fields.rb
+++ db/migrate/20131031034929_modify_submission_fields.rb
@@ -1,7 +1,6 @@
 class ModifySubmissionFields < ActiveRecord::Migration
   def change
-    remove_column :submissions, :user_id, :integer
-    add_column :submissions, :student_id, :integer
+    add_column :submissions, :user_id, :integer
     add_column :submissions, :grader_id, :string
   end
 end
